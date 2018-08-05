14823da2bbc4e9de8c4f0e2c56d61b671fcd3566
Britone Mwasaru <bmwasaru@gmail.com>
2018-08-05 17:18:04 +0300

    Update potpesa.php

--- potpesa.php
+++ potpesa.php
@@ -67,3 +67,81 @@ function potpesa_action_links( $links )
 {
 	return array_merge( $links, [ '<a href="'.admin_url( 'admin.php?page=page=potpesa' ).'">&nbsp;Configure</a>' ] );
 }
+
+$mconfig = get_option( 'mc_options' );
+$mconfig['mc_conf_callback_url']     = rtrim( home_url(), '/').':443/?mpesa_ipn_listener=reconcile';
+$mconfig['mc_conf_timeout_url']      = rtrim( home_url(), '/').':443/?mpesa_ipn_listener=timeout';
+$mconfig['mc_conf_result_url'] 		  = rtrim( home_url(), '/').':443/?mpesa_ipn_listener=reconcile';
+$mconfig['mc_conf_confirmation_url'] = rtrim( home_url(), '/').':443/?mpesa_ipn_listener=confirm';
+$mconfig['mc_conf_validation_url'] 	= rtrim( home_url(), '/').':443/?mpesa_ipn_listener=validate';
+stk_config( $mconfig );
+
+add_shortcode('POTPAYER', 'mc_form_callback');
+function mc_form_callback( $atts = array(), $content = null ) {
+	$mconfig = get_option( 'mc_options' );
+	$status = isset( $_SESSION['mc_trx_status'] ) ? $mconfig['mc_mpesa_conf_msg'].'<br>'.$_SESSION['mc_trx_status'] : '';
+  return '<form id="mc-contribution-form" method="POST" action="" class="mc_contribution_form">
+  	<p>'.$status.'</p>
+  	<input type="hidden" name="action" value="process_mc_form">
+    
+  	<label for="mc-phone">Phone Number</label>
+  	<input id="mc-phone" type="text" name="mc-phone" placeholder="Phone Number" class="mc_phone"><br>
+  	<label for="mc-amount">Amount to contribute</label>
+  	<input id="mc-amount" type="text" name="mc-amount" value="200" class="mc_amount"><br>
+  	<button type="submit" name="mc-contribute" class="mc_contribute">CONTRIBUTE</button>
+  </form>';
+}
+add_action( 'init', 'mc_process_form_data' );
+function mc_process_form_data() {
+  if ( isset( $_POST['mc-contribute'] ) ) {
+    $amount   = trim( $_POST['mc-amount'] );
+  	$phone 		= trim( $_POST['mc-phone'] );
+  	$response 	= mc_mpesa_checkout( $amount, $phone, 'Contributions' );
+  	$status 	= json_decode( $response );
+	$s 			= '';  }
+}
+/**
+ * Register Validation and Confirmation URLs
+ * Outputs registration status
+ */
+add_action( 'init', 'mc_mpesa_do_register' );
+function mc_mpesa_do_register()
+{
+	header( "Access-Control-Allow-Origin: *" );
+	header( 'Content-Type:Application/json' );
+	if ( ! isset( $_GET['mpesa_ipn_register'] ) ){ return; }
+    
+	wp_send_json( stk_register() );
+}
+/**
+ * 
+ */
+add_action( 'init', 'mc_mpesa_confirm' );
+function mc_mpesa_confirm()
+{
+	if ( ! isset( $_GET['mpesa_ipn_listener'] ) ) return;
+    if ( $_GET['mpesa_ipn_listener'] !== 'confirm' ) return;
+	$response = json_decode( file_get_contents( 'php://input' ), true );
+	if( ! isset( $response['Body'] ) ){
+    	return;
+    }
+	header( "Access-Control-Allow-Origin: *" );
+	header( 'Content-Type:Application/json' );
+	wp_send_json( stk_confirm() );
+}
+/**
+ * 
+ */
+add_action( 'init', 'mc_mpesa_validate' );
+function mc_mpesa_validate()
+{
+	if ( ! isset( $_GET['mpesa_ipn_listener'] ) ){ return; }
+    if ( $_GET['mpesa_ipn_listener'] !== 'validate' ){ return; }
+	$response = json_decode( file_get_contents( 'php://input' ), true );
+	if( ! isset( $response['Body'] ) ){
+    	return;
+    }
+	header( "Access-Control-Allow-Origin: *" );
+	header( 'Content-Type:Application/json' );
+	wp_send_json( stk_validate() );
+}
